8acb314739401eadfed6a09ad41c391ae893ec40
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-05-18 14:19:11 +0100

    bugfixes

--- src/danog/Serialization.php
+++ src/danog/Serialization.php
@@ -17,7 +17,7 @@ class Serialization
     public static function unserialize($data)
     {
         foreach (get_declared_classes() as $class) {
-            if (isset(class_uses($class)['danog\Serializable']) || $class === 'Volatile') {
+            if (isset(class_uses($class)['danog\Serializable'])) {
                 $namelength = strlen($class);
                 $data = explode('O:'.$namelength.':"'.$class.'":', $data);
                 $stringdata = array_shift($data);
